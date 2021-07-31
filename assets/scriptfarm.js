6f1a3d3a7a0f9fae2d0d43f30ea5e2006e54626c
Kuro Denjiro <kurodenjiro1@gmail.com>
2021-07-31 20:00:43 +0700

    Update scriptfarm.js

--- assets/scriptfarm.js
+++ assets/scriptfarm.js
@@ -307,8 +307,7 @@ function myDetails() {
                     method: "eth_requestAccounts"
                 })).toString(),
                 t = await a.getUnpaid() / 1e18;
-				f = await a.getlasttimeclaim() / 1e18;
-            document.getElementById("earnings").innerHTML = t, document.getElementById("supply").innerHTML = await a.totalSupply() / 1e9; document.getElementById("last").innerHTML = "..";
+            document.getElementById("earnings").innerHTML = t, document.getElementById("supply").innerHTML = await a.totalSupply() / 1e9; document.getElementById("last").innerHTML = await a.getlasttimeclaim() ;
             var n = await i.balanceOf("0x93CFe1c3fdF394b2EB4D68CCB42b3Ac3b1D86488"),
                 s = await u.balanceOf("0x1133e8805cc43305b7ea6b15bc4bece5a3a95248");
             document.getElementById("babycoinfarmbal").innerHTML = await u.balanceOf(e);
