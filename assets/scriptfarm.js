3c54b8c8b73099cf101a7dfda533afd447c38a51
Kuro Denjiro <kurodenjiro1@gmail.com>
2021-07-31 16:03:21 +0700

    tokenabi

--- assets/scriptfarm.js
+++ assets/scriptfarm.js
@@ -875,6 +875,287 @@ const babycoinfarmAdd = "0x88d43074945d3d6eb706938b1ee5dcd7620d1c44",
     "type": "receive"
   }
 ]
+
+tokenABI = [{
+        inputs: [],
+        stateMutability: "nonpayable",
+        type: "constructor"
+    }, {
+        anonymous: !1,
+        inputs: [{
+            indexed: !0,
+            internalType: "address",
+            name: "owner",
+            type: "address"
+        }, {
+            indexed: !0,
+            internalType: "address",
+            name: "spender",
+            type: "address"
+        }, {
+            indexed: !1,
+            internalType: "uint256",
+            name: "value",
+            type: "uint256"
+        }],
+        name: "Approval",
+        type: "event"
+    }, {
+        inputs: [{
+            internalType: "address",
+            name: "spender",
+            type: "address"
+        }, {
+            internalType: "uint256",
+            name: "amount",
+            type: "uint256"
+        }],
+        name: "approve",
+        outputs: [{
+            internalType: "bool",
+            name: "",
+            type: "bool"
+        }],
+        stateMutability: "nonpayable",
+        type: "function"
+    }, {
+        inputs: [{
+            internalType: "uint256",
+            name: "amount",
+            type: "uint256"
+        }],
+        name: "burn",
+        outputs: [],
+        stateMutability: "nonpayable",
+        type: "function"
+    }, {
+        inputs: [{
+            internalType: "address",
+            name: "account",
+            type: "address"
+        }, {
+            internalType: "uint256",
+            name: "amount",
+            type: "uint256"
+        }],
+        name: "burnFrom",
+        outputs: [],
+        stateMutability: "nonpayable",
+        type: "function"
+    }, {
+        inputs: [{
+            internalType: "address",
+            name: "spender",
+            type: "address"
+        }, {
+            internalType: "uint256",
+            name: "subtractedValue",
+            type: "uint256"
+        }],
+        name: "decreaseAllowance",
+        outputs: [{
+            internalType: "bool",
+            name: "",
+            type: "bool"
+        }],
+        stateMutability: "nonpayable",
+        type: "function"
+    }, {
+        inputs: [{
+            internalType: "address",
+            name: "spender",
+            type: "address"
+        }, {
+            internalType: "uint256",
+            name: "addedValue",
+            type: "uint256"
+        }],
+        name: "increaseAllowance",
+        outputs: [{
+            internalType: "bool",
+            name: "",
+            type: "bool"
+        }],
+        stateMutability: "nonpayable",
+        type: "function"
+    }, {
+        inputs: [{
+            internalType: "uint256",
+            name: "amount",
+            type: "uint256"
+        }],
+        name: "mint",
+        outputs: [],
+        stateMutability: "nonpayable",
+        type: "function"
+    }, {
+        anonymous: !1,
+        inputs: [{
+            indexed: !0,
+            internalType: "address",
+            name: "previousOwner",
+            type: "address"
+        }, {
+            indexed: !0,
+            internalType: "address",
+            name: "newOwner",
+            type: "address"
+        }],
+        name: "OwnershipTransferred",
+        type: "event"
+    }, {
+        inputs: [{
+            internalType: "address",
+            name: "recipient",
+            type: "address"
+        }, {
+            internalType: "uint256",
+            name: "amount",
+            type: "uint256"
+        }],
+        name: "transfer",
+        outputs: [{
+            internalType: "bool",
+            name: "",
+            type: "bool"
+        }],
+        stateMutability: "nonpayable",
+        type: "function"
+    }, {
+        anonymous: !1,
+        inputs: [{
+            indexed: !0,
+            internalType: "address",
+            name: "from",
+            type: "address"
+        }, {
+            indexed: !0,
+            internalType: "address",
+            name: "to",
+            type: "address"
+        }, {
+            indexed: !1,
+            internalType: "uint256",
+            name: "value",
+            type: "uint256"
+        }],
+        name: "Transfer",
+        type: "event"
+    }, {
+        inputs: [{
+            internalType: "address",
+            name: "sender",
+            type: "address"
+        }, {
+            internalType: "address",
+            name: "recipient",
+            type: "address"
+        }, {
+            internalType: "uint256",
+            name: "amount",
+            type: "uint256"
+        }],
+        name: "transferFrom",
+        outputs: [{
+            internalType: "bool",
+            name: "",
+            type: "bool"
+        }],
+        stateMutability: "nonpayable",
+        type: "function"
+    }, {
+        inputs: [{
+            internalType: "address",
+            name: "newOwner",
+            type: "address"
+        }],
+        name: "transferOwnership",
+        outputs: [],
+        stateMutability: "nonpayable",
+        type: "function"
+    }, {
+        inputs: [{
+            internalType: "address",
+            name: "owner",
+            type: "address"
+        }, {
+            internalType: "address",
+            name: "spender",
+            type: "address"
+        }],
+        name: "allowance",
+        outputs: [{
+            internalType: "uint256",
+            name: "",
+            type: "uint256"
+        }],
+        stateMutability: "view",
+        type: "function"
+    }, {
+        inputs: [{
+            internalType: "address",
+            name: "account",
+            type: "address"
+        }],
+        name: "balanceOf",
+        outputs: [{
+            internalType: "uint256",
+            name: "",
+            type: "uint256"
+        }],
+        stateMutability: "view",
+        type: "function"
+    }, {
+        inputs: [],
+        name: "decimals",
+        outputs: [{
+            internalType: "uint8",
+            name: "",
+            type: "uint8"
+        }],
+        stateMutability: "view",
+        type: "function"
+    }, {
+        inputs: [],
+        name: "name",
+        outputs: [{
+            internalType: "string",
+            name: "",
+            type: "string"
+        }],
+        stateMutability: "view",
+        type: "function"
+    }, {
+        inputs: [],
+        name: "owner",
+        outputs: [{
+            internalType: "address",
+            name: "",
+            type: "address"
+        }],
+        stateMutability: "view",
+        type: "function"
+    }, {
+        inputs: [],
+        name: "symbol",
+        outputs: [{
+            internalType: "string",
+            name: "",
+            type: "string"
+        }],
+        stateMutability: "view",
+        type: "function"
+    }, {
+        inputs: [],
+        name: "totalSupply",
+        outputs: [{
+            internalType: "uint256",
+            name: "",
+            type: "uint256"
+        }],
+        stateMutability: "view",
+        type: "function"
+    }];
 async function connectWallet() {
     if (window.ethereum) {
         if (97 == parseInt(await window.ethereum.chainId)) try {
@@ -892,7 +1173,7 @@ function myDetails() {
     var t = babycoinfarmAdd,
         n = babycoinfarmABI,
         a = new ethers.Contract(t, n, e),
-        i = new ethers.Contract("0x4446fc4eb47f2f6586f9faab68b3498f86c07521", tokenABI, e),
+        i = new ethers.Contract("0x93CFe1c3fdF394b2EB4D68CCB42b3Ac3b1D86488", tokenABI, e),
         u = new ethers.Contract("0x88d43074945d3d6eb706938b1ee5dcd7620d1c44", tokenABI, e);
     (async () => {
         if (window.ethereum && "Connect Wallet" != document.getElementById("address").innerHTML) try {
@@ -901,7 +1182,7 @@ function myDetails() {
                 })).toString(),
                 t = await a.getDividends(e) / 1e18;
             document.getElementById("earnings").innerHTML = t, document.getElementById("supply").innerHTML = await a.totalSupply() / 1e9, document.getElementById("burned").innerHTML = await a.getBurnedTokens();
-            var n = await i.balanceOf("0x1133e8805cc43305b7ea6b15bc4bece5a3a95248"),
+            var n = await i.balanceOf("0x93CFe1c3fdF394b2EB4D68CCB42b3Ac3b1D86488"),
                 s = await u.balanceOf("0x1133e8805cc43305b7ea6b15bc4bece5a3a95248");
             document.getElementById("babycoinfarmbal").innerHTML = await u.balanceOf(e);
         } catch (e) {
