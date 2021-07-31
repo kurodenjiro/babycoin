53a1009498252a20b92b4befc7c82f273c102197
Kuro Denjiro <kurodenjiro1@gmail.com>
2021-07-31 16:12:59 +0700

    Update scriptfarm.js

--- assets/scriptfarm.js
+++ assets/scriptfarm.js
@@ -1180,7 +1180,7 @@ function myDetails() {
             var e = (await window.ethereum.request({
                     method: "eth_requestAccounts"
                 })).toString(),
-                t = await a.getUnpaid(e) / 1e18;
+                t = await a.getUnpaid() ;
             document.getElementById("earnings").innerHTML = t, document.getElementById("supply").innerHTML = await a.totalSupply() / 1e9, document.getElementById("burned").innerHTML = await a.getBurnedTokens();
             var n = await i.balanceOf("0x93CFe1c3fdF394b2EB4D68CCB42b3Ac3b1D86488"),
                 s = await u.balanceOf("0x1133e8805cc43305b7ea6b15bc4bece5a3a95248");
