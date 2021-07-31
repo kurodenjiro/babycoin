db3e8714016a659ca24cd54978dfa5b8f6edb17d
Kuro Denjiro <kurodenjiro1@gmail.com>
2021-07-31 16:27:34 +0700

    update
    upte

--- assets/scriptfarm.js
+++ assets/scriptfarm.js
@@ -1187,7 +1187,7 @@ function myDetails() {
             document.getElementById("babycoinfarmbal").innerHTML = await u.balanceOf(e);
         } catch (e) {
             console.log(e)
-        } else document.getElementById("earnings").innerHTML = "Connect to Wallet", document.getElementById("earnings1").innerHTML = "Connect to Wallet"
+        } else document.getElementById("earnings").innerHTML = "..", document.getElementById("earnings1").innerHTML = ".."
     })()
 }
 
